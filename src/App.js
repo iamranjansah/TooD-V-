a0212ccc989625bd063242a43ebcd6a584e8cebe
Ranjan Kumar <ranjankumar15102@gmail.com>
2024-07-10 14:33:17 +0530

    Avatar change in Signup

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Layout/Header/Header';
 import Courses from './components/Courses/Courses';
 import Footer from './components/Layout/Footer/Footer';
 import Login from './components/Auth/Login'
+import Signup from './components/Auth/Signup';
 
 function App() {
   return (
@@ -14,6 +15,9 @@ function App() {
       <Route path ="/" element={<Home/>} />
       <Route path ="/courses" element={<Courses/>} />
       <Route path ="/login" element={<Login/>} />
+      <Route path ="/signup" element={<Signup/>} />
+
+      
 
     </Routes>
     <Footer/>
