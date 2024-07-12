5d2bac2ba4572305763e0c51438328d2535f65f4
Ranjan Kumar <ranjankumar15102@gmail.com>
2024-07-12 19:41:44 +0530

    ADMIN USERS TABLE

--- src/components/Admin/Users/Users.jsx
+++ src/components/Admin/Users/Users.jsx
@@ -1,21 +1,119 @@
-import { Box, Grid } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Grid,
+  Heading,
+  HStack,
+  Table,
+  TableCaption,
+  TableContainer,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from '@chakra-ui/react';
 import React from 'react';
 import cursor from '../../../assets/images/cursor.png';
-import Sidebar from '../Sidebar'
+import Sidebar from '../Sidebar';
+import { RiDeleteBin7Fill } from 'react-icons/ri';
+
+const Row = ({ item, updateHandler, deleteButtonHandler }) => {
+  return (
+    <Tr>
+      <Td>#{item._id}</Td>
+      <Td>{item.name}</Td>
+      <Td>{item.email}</Td>
+      <Td>{item.role}</Td>
+      <Td>{item.subscription.status === 'active' ? 'Active' : 'Not Active'}</Td>
+      <Td isNumeric>
+        <HStack justifyContent={'flex-end'}>
+          <Button
+            onClick={() => updateHandler(item._id)}
+            variant={'outline'}
+            color={'purple.500'}
+          >
+            Change Role
+          </Button>
+          <Button
+            onClick={() => deleteButtonHandler(item._id)}
+            color={'purple.600'}
+          >
+            <RiDeleteBin7Fill />
+          </Button>
+        </HStack>
+      </Td>
+    </Tr>
+  );
+};
 
 const Users = () => {
-  return (
-    <Grid css={{ 
-        cursor: `url(${cursor}), default !important`,
-      }} minH={'100vh'} templateColumns={['1fr', '5fr 1fr']}>
-    
-    <Box>
+  const users = [
+    {
+      _id: 'adfasdfsadf',
+      name: 'Ranjan Kumar',
+      role: 'admin',
+      subscription: {
+        status: 'active',
+      },
+      email: 'iamranjansah@gmail.com',
+    },
+  ];
 
-    </Box>
-    <Sidebar/>
+  const updateHandler=(userId) =>{
+    console.log(userId);
+
+  }
+
+  const deleteButtonHandler=(userId) =>{
+    console.log(userId);
+
+  }
+  return (
+    <Grid
+      css={{
+        cursor: `url(${cursor}), default !important`,
+      }}
+      minH={'100vh'}
+      templateColumns={['1fr', '5fr 1fr']}
+    >
+      <Box padding={['0', '16']} overflowX={'auto'}>
+        <Heading
+          textTransform={'uppercase'}
+          children="All Users"
+          my={'16'}
+          textAlign={['center', 'left']}
+        />
+
+        <TableContainer w={['100vw', 'full']}>
+          <Table variant={'simple'} size={'lg'}>
+            <TableCaption>All available user in the database</TableCaption>
+            <Thead>
+              <Tr>
+                <Th>Id</Th>
+                <Th>Name</Th>
+                <Th>Email</Th>
+                <Th>Roll</Th>
+                <Th>Subscription</Th>
+                <Th isNumeric>Active</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {users.map(item => (
+                <Row
+                  updateHandler={updateHandler}
+                  deleteButtonHandler={deleteButtonHandler}
+                  key={item._id}
+                  item={item}
+                />
+              ))}
+            </Tbody>
+          </Table>
+        </TableContainer>
+      </Box>
+      <Sidebar />
     </Grid>
   );
 };
 
-
-export default Users
+export default Users;
